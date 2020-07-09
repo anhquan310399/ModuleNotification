ee2e0becd93f3630a8346aa74e63a2ff96df82f5
IBeaconNotify <anhquan310399@gmail.com>
2020-07-09 09:46:11 +0700

    Initial Commit

---
+++ ModuleNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModuleNotification
+//
+//  Created by cntt37 on 7/9/20.
+//  Copyright © 2020 hcmute.edu.vn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
